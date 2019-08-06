c82b39f7266d49e5a1d0db547a738d051bda7227
CTEmpowering <falusiabayomi@gmail.com>
2019-08-06 04:59:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/PayApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/PayApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/PayApp/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
